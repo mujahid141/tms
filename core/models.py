1271aae792cce0505d8691a238a297306227512f
Mujahid Anwar <mujahidanwar141@gmail.com>
2024-02-07 20:33:31 +0500

    Project Started

---
+++ core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class User(models.Model):
+    userName = models.CharField(primary_key=True)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+class Category(models.Model):
+    title = models.CharField(max_length=40, primary_key=True)
+    discription =  models.CharField(max_length=255, null= True)
+    
+
+class Task(models.Model):
+    taskId = models.UUIDField(auto_created=True)
+    taskName = models.CharField(max_length= 255)
+    discription = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    startingDate = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
+    
